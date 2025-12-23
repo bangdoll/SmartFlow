bf782b2c37cb5019161e5cbef53e97d7e8c79bfd
bangdoll <bangdoll@example.com>
2025-12-23 22:59:39 +0800

    feat: Add CORS headers for feed.xml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/feed.xml',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
